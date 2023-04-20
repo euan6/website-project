859bb0fdae984a8b7a43e52cabc375c616e55040
Euan <euanconlon46@gmail.com>
2023-04-20 23:00:07 +0100

    Updated indexing AddReview.php

--- AddReview.php
+++ AddReview.php
@@ -20,85 +20,85 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" href="images/favicon.ico">
 </head>
-    <body>
-		
-		<div id="banner"> <!-- header banner -->
-			<header>
-				<h1>Programming Reviews</h1>
-			</header>
+<body>
+	
+	<div id="banner"> <!-- header banner -->
+		<header>
+			<h1>Programming Reviews</h1>
+		</header>
+	</div>
+	
+	<nav> <!-- nav bar -->
+		<ul>
+			<li><a href="home.php">Home Page</a></li>
+			<li><a href="readReviews.php">Read Review</a></li>
+			<li><a class="active" href="AddReview.php">Add Review</a></li>
+			<li><a href="searchReview.php">Search</a></li>
+			<li><a href="signUp.php">Sign Up</a></li>
+			<li><a href="logIn.php">Log In</a></li>
+		</ul>
+	</nav>
+	
+	<main>
+		<div id="area1"> <!-- main area -->
+			<h1>Add Reviews</h1>
+
+			<?php
+			//if a user is logged in then display content
+			if ($_SESSION['userLogin'] == true){
+			echo '<p>Use the form below to write and submit your own review.</p>
+					<div class="addform">
+					<form action="AddReview2.php" method="POST">
+						<p>Review Language:</p>
+						<input type="text" name="reviewLanguage" maxlength="20" placeholder="Please enter your review language here" autocomplete="off" required>
+						<br><br>
+						<p>Review Text:</p>
+						<input type="text" name="reviewText" maxlength="250" placeholder="Please enter your review text here" autocomplete="off" required>
+						<br><br>
+						<p>Review Rating:</p>
+						<input type="number" name="reviewRating" min="1" max="5" placeholder="1" autocomplete="off" required>
+						<input type="submit" value="Submit">             
+					</form>
+					</div>';
+			//else display error message saying you must be logged in
+			} else {
+				echo '<p>You must be logged in to view this content!</p>';
+			}
+			?>
+
+		</div>
+	</main>
+	
+	<footer>
+		<div class="col1"> <!-- footer download column -->
+			<h2>Download our App</h2>
+			<ul>
+				<li>Download on the app store or google play store</li>
+			</ul>
+			<div class="app-logo">
+				<img src="images/play-store.png" alt="playstore.png">
+				<img src="images/app-store.png" alt="appstore.png">
+			</div>		
 		</div>
 		
-		<nav> <!-- nav bar -->
+		<div class="col2"> <!-- footer contact column -->
+			<h2>Contact us</h2>
 			<ul>
-				<li><a href="home.php">Home Page</a></li>
-				<li><a href="readReviews.php">Read Review</a></li>
-				<li><a class="active" href="AddReview.php">Add Review</a></li>
-				<li><a href="searchReview.php">Search</a></li>
-				<li><a href="signUp.php">Sign Up</a></li>
-				<li><a href="logIn.php">Log In</a></li>
+				<li>Tel: 0131 345 6789</li>
+				<li>Fax: 0131 987 6543</li>
+				<li>Email: programming@reviews.com</li>
 			</ul>
-		</nav>
+		</div>
 		
-		<main>
-			<div id="area1"> <!-- main area -->
-				<h1>Add Reviews</h1>
-
-				<?php
-				//if a user is logged in then display content
-				if ($_SESSION['userLogin'] == true){
-				echo '<p>Use the form below to write and submit your own review.</p>
-        			  <div class="addform">
-          				<form action="AddReview2.php" method="POST">
-              				<p>Review Language:</p>
-              				<input type="text" name="reviewLanguage" maxlength="20" placeholder="Please enter your review language here" autocomplete="off" required>
-              				<br><br>
-              				<p>Review Text:</p>
-              				<input type="text" name="reviewText" maxlength="250" placeholder="Please enter your review text here" autocomplete="off" required>
-              				<br><br>
-              				<p>Review Rating:</p>
-              				<input type="number" name="reviewRating" min="1" max="5" placeholder="1" autocomplete="off" required>
-              				<input type="submit" value="Submit">             
-          				</form>
-        			  </div>';
-				//else display error message saying you must be logged in
-				} else {
-					echo '<p>You must be logged in to view this content!</p>';
-				}
-				?>
-
-			</div>
-		</main>
-		
-		<footer>
-			<div class="col1"> <!-- footer download column -->
-				<h2>Download our App</h2>
-				<ul>
-				    <li>Download on the app store or google play store</li>
-				</ul>
-				<div class="app-logo">
-				    <img src="images/play-store.png" alt="playstore.png">
-				    <img src="images/app-store.png" alt="appstore.png">
-				</div>		
-			</div>
-			
-			<div class="col2"> <!-- footer contact column -->
-			    <h2>Contact us</h2>
-			    <ul>
-				    <li>Tel: 0131 345 6789</li>
-				    <li>Fax: 0131 987 6543</li>
-				    <li>Email: programming@reviews.com</li>
-			    </ul>
-			</div>
-			
-			<div class="col3"> <!-- footer socials column -->
-			    <h2>Follow us</h2>
-			    <ul>
-				    <li class="fb">Facebook</li>
-				    <li class="tw">Twitter</li>
-				    <li class="in">Instagram</li>
-				    <li class="yt">YouTube</li>
-			    </ul>
-			</div>
-		</footer>
-    </body>
+		<div class="col3"> <!-- footer socials column -->
+			<h2>Follow us</h2>
+			<ul>
+				<li class="fb">Facebook</li>
+				<li class="tw">Twitter</li>
+				<li class="in">Instagram</li>
+				<li class="yt">YouTube</li>
+			</ul>
+		</div>
+	</footer>
+</body>
 </html>
