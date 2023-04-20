cfc1cd627a14a9171785ee15052fab6f67009d86
Euan <euanconlon46@gmail.com>
2023-04-20 22:57:25 +0100

    Fixed indexing on "addreview"

--- AddReview.php
+++ AddReview.php
@@ -15,9 +15,9 @@
 <head>
 	<title>Programming Reviews | Add Reviews</title>
 	<link href="projectStyle.css" type="text/css"	rel="stylesheet">
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" href="images/favicon.ico">
 </head>
     <body>
@@ -47,19 +47,19 @@
 				//if a user is logged in then display content
 				if ($_SESSION['userLogin'] == true){
 				echo '<p>Use the form below to write and submit your own review.</p>
-        <div class="addform">
-          <form action="AddReview2.php" method="POST">
-              <p>Review Language:</p>
-              <input type="text" name="reviewLanguage" maxlength="20" placeholder="Please enter your review language here" autocomplete="off" required>
-              <br><br>
-              <p>Review Text:</p>
-              <input type="text" name="reviewText" maxlength="250" placeholder="Please enter your review text here" autocomplete="off" required>
-              <br><br>
-              <p>Review Rating:</p>
-              <input type="number" name="reviewRating" min="1" max="5" placeholder="1" autocomplete="off" required>
-              <input type="submit" value="Submit">             
-          </form>
-        </div>';
+        			  <div class="addform">
+          				<form action="AddReview2.php" method="POST">
+              				<p>Review Language:</p>
+              				<input type="text" name="reviewLanguage" maxlength="20" placeholder="Please enter your review language here" autocomplete="off" required>
+              				<br><br>
+              				<p>Review Text:</p>
+              				<input type="text" name="reviewText" maxlength="250" placeholder="Please enter your review text here" autocomplete="off" required>
+              				<br><br>
+              				<p>Review Rating:</p>
+              				<input type="number" name="reviewRating" min="1" max="5" placeholder="1" autocomplete="off" required>
+              				<input type="submit" value="Submit">             
+          				</form>
+        			  </div>';
 				//else display error message saying you must be logged in
 				} else {
 					echo '<p>You must be logged in to view this content!</p>';
@@ -70,37 +70,35 @@
 		</main>
 		
 		<footer>
-		
 			<div class="col1"> <!-- footer download column -->
 				<h2>Download our App</h2>
 				<ul>
-					<li>Download on the app store or google play store</li>
+				    <li>Download on the app store or google play store</li>
 				</ul>
 				<div class="app-logo">
-					<img src="images/play-store.png" alt="playstore.png">
-					<img src="images/app-store.png" alt="appstore.png">
+				    <img src="images/play-store.png" alt="playstore.png">
+				    <img src="images/app-store.png" alt="appstore.png">
 				</div>		
 			</div>
 			
 			<div class="col2"> <!-- footer contact column -->
-			  <h2>Contact us</h2>
-			  <ul>
-					<li>Tel: 0131 345 6789</li>
-					<li>Fax: 0131 987 6543</li>
-					<li>Email: programming@reviews.com</li>
-			  </ul>
+			    <h2>Contact us</h2>
+			    <ul>
+				    <li>Tel: 0131 345 6789</li>
+				    <li>Fax: 0131 987 6543</li>
+				    <li>Email: programming@reviews.com</li>
+			    </ul>
 			</div>
 			
 			<div class="col3"> <!-- footer socials column -->
-			  <h2>Follow us</h2>
-			  <ul>
-					<li class="fb">Facebook</li>
-					<li class="tw">Twitter</li>
-					<li class="in">Instagram</li>
-					<li class="yt">YouTube</li>
-			  </ul>
+			    <h2>Follow us</h2>
+			    <ul>
+				    <li class="fb">Facebook</li>
+				    <li class="tw">Twitter</li>
+				    <li class="in">Instagram</li>
+				    <li class="yt">YouTube</li>
+			    </ul>
 			</div>
 		</footer>
-    
-  </body>
+    </body>
 </html>
